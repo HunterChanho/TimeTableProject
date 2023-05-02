513333f2d898d590af1584b1dec26986841a26ea
Yanghewon <hewon8883@naver.com>
2023-05-02 23:42:46 +0900

    dd

---
+++ TimetableProject/src/test/java/com/example/TimetableProject/TimetableProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TimetableProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
